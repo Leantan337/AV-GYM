9236252fcaf9777de67ff3fd05506b4b9a15bf5a
alula <natnael337@gmail.com>
2025-04-09 11:09:40 +0300

    transaltion fix stage to deploy

--- fitcore/translation/middleware.py
+++ fitcore/translation/middleware.py
@@ -6,11 +6,12 @@ class LanguageMiddleware(MiddlewareMixin):
     Middleware that sets the language based on URL parameter
     """
     def process_request(self, request):
-        language = request.GET.get('lang', None)
-        if language:
-            if language in ['en', 'es']:  # Only allow supported languages
-                translation.activate(language)
-                request.LANGUAGE_CODE = translation.get_language()
+        language = request.GET.get('lang', 'en')  # Default to English
+        if language in ['en', 'es', 'am', 'ti']:  # Only allow supported languages
+            translation.activate(language)
+            request.LANGUAGE_CODE = translation.get_language()
+            request.GET = request.GET.copy()  # Make GET mutable
+            request.GET['lang'] = language  # Ensure lang is always in GET
 
     def process_response(self, request, response):
         language = request.GET.get('lang', None)
